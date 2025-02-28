b7a58512eb42cc1075d167881c337414aaf7288b
Himanshu Verma <hv65616@gmail.com>
2025-02-28 20:52:55 +0530

    url change

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://pr-24.d3fzj4oag4giy.amplifyapp.com",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
